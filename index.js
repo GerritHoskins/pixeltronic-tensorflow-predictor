172cebc09224f34e24cadcbdab67a911c5dbcc9b
Gerrit Hoskins <39583066+GerritHoskins@users.noreply.github.com>
2023-11-24 09:15:58 +0000

    kh7

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {  
-  let mobilenet = undefined;
-  let gatherDataState = "STOP_DATA_GATHER";
-  let videoPlaying = false;
-  let trainingDataInputs = [];
-  let trainingDataOutputs = [];
-  let examplesCount = [];
-  let predict = false;
-
+document.addEventListener('DOMContentLoaded', async () => {    
   const STATUS = document.getElementById('status');
   const VIDEO = document.getElementById('webcam');
   const ENABLE_CAM_BUTTON = document.getElementById('enableCam');
@@ -17,21 +9,132 @@ document.addEventListener('DOMContentLoaded', () => {
   const STOP_DATA_GATHER = -1;
   const CLASS_NAMES = [];
 
+  let mobilenet = undefined;
+  let gatherDataState = STOP_DATA_GATHER;
+  let videoPlaying = false;
+  let trainingDataInputs = [];
+  let trainingDataOutputs = [];
+  let examplesCount = [];
+  let predict = false;
+
+  function hasGetUserMedia() {
+    return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+  }
+  
   function enableCam() {
-    // TODO: Fill this out later in the codelab!
+    if (hasGetUserMedia()) {
+      // getUsermedia parameters.
+      const constraints = {
+        video: true,
+        width: 640, 
+        height: 480 
+      };
+  
+      // Activate the webcam stream.
+      navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+        VIDEO.srcObject = stream;
+        VIDEO.addEventListener('loadeddata', function() {
+          videoPlaying = true;
+          ENABLE_CAM_BUTTON.classList.add('removed');
+        });
+      });
+    } else {
+      console.warn('getUserMedia() is not supported by your browser');
+    }
   }
 
-  function trainAndPredict() {
-    // TODO: Fill this out later in the codelab!
-  }
-
-
-  function reset() {
-    // TODO: Fill this out later in the codelab!
-  }
-
+  /**
+   * Handle Data Gather for button mouseup/mousedown.
+   **/
   function gatherDataForClass() {
-    // TODO: Fill this out later in the codelab!
+    let classNumber = parseInt(this.getAttribute('data-1hot'));
+    gatherDataState = (gatherDataState === STOP_DATA_GATHER) ? classNumber : STOP_DATA_GATHER;
+    dataGatherLoop();
+  }
+
+  function dataGatherLoop() {
+    if (videoPlaying && gatherDataState !== STOP_DATA_GATHER) {
+      let imageFeatures = tf.tidy(function() {
+        let videoFrameAsTensor = tf.browser.fromPixels(VIDEO);
+        let resizedTensorFrame = tf.image.resizeBilinear(videoFrameAsTensor, [MOBILE_NET_INPUT_HEIGHT, 
+            MOBILE_NET_INPUT_WIDTH], true);
+        let normalizedTensorFrame = resizedTensorFrame.div(255);
+        return mobilenet.predict(normalizedTensorFrame.expandDims()).squeeze();
+      });
+  
+      trainingDataInputs.push(imageFeatures);
+      trainingDataOutputs.push(gatherDataState);
+      
+      // Intialize array index element if currently undefined.
+      if (examplesCount[gatherDataState] === undefined) {
+        examplesCount[gatherDataState] = 0;
+      }
+      examplesCount[gatherDataState]++;
+  
+      STATUS.innerText = '';
+      for (let n = 0; n < CLASS_NAMES.length; n++) {
+        STATUS.innerText += CLASS_NAMES[n] + ' data count: ' + examplesCount[n] + '. ';
+      }
+      window.requestAnimationFrame(dataGatherLoop);
+    }
+  }
+
+  async function trainAndPredict() {
+    predict = false;
+    tf.util.shuffleCombo(trainingDataInputs, trainingDataOutputs);
+    let outputsAsTensor = tf.tensor1d(trainingDataOutputs, 'int32');
+    let oneHotOutputs = tf.oneHot(outputsAsTensor, CLASS_NAMES.length);
+    let inputsAsTensor = tf.stack(trainingDataInputs);
+    
+    let results = await model.fit(inputsAsTensor, oneHotOutputs, {shuffle: true, batchSize: 5, epochs: 10, 
+        callbacks: {onEpochEnd: logProgress} });
+    
+    outputsAsTensor.dispose();
+    oneHotOutputs.dispose();
+    inputsAsTensor.dispose();
+    predict = true;
+    predictLoop();
+  }
+
+  function predictLoop() {
+    if (predict) {
+      tf.tidy(function() {
+        let videoFrameAsTensor = tf.browser.fromPixels(VIDEO).div(255);
+        let resizedTensorFrame = tf.image.resizeBilinear(videoFrameAsTensor,[MOBILE_NET_INPUT_HEIGHT, 
+            MOBILE_NET_INPUT_WIDTH], true);
+  
+        let imageFeatures = mobilenet.predict(resizedTensorFrame.expandDims());
+        let prediction = model.predict(imageFeatures).squeeze();
+        let highestIndex = prediction.argMax().arraySync();
+        let predictionArray = prediction.arraySync();
+  
+        STATUS.innerText = 'Prediction: ' + CLASS_NAMES[highestIndex] + ' with ' + Math.floor(predictionArray[highestIndex] * 100) + '% confidence';
+      });
+  
+      window.requestAnimationFrame(predictLoop);
+    }
+  }
+  
+  function logProgress(epoch, logs) {
+    console.log('Data for epoch ' + epoch, logs);
+  }
+
+  /**
+ * Purge data and start over. Note this does not dispose of the loaded 
+ * MobileNet model and MLP head tensors as you will need to reuse 
+ * them to train a new model.
+ **/
+  function reset() {
+    predict = false;
+    examplesCount.length = 0;
+    for (let i = 0; i < trainingDataInputs.length; i++) {
+      trainingDataInputs[i].dispose();
+    }
+    trainingDataInputs.length = 0;
+    trainingDataOutputs.length = 0;
+    STATUS.innerText = 'No data collected';
+    
+    console.log('Tensors in memory: ' + tf.memory().numTensors);
   }
 
   ENABLE_CAM_BUTTON.addEventListener('click', enableCam);
@@ -60,7 +163,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
   // Call the function immediately to start loading.
-  loadMobileNetFeatureModel();  
+  await loadMobileNetFeatureModel();  
 
   let model = tf.sequential();
   model.add(tf.layers.dense({inputShape: [1024], units: 128, activation: 'relu'}));
