5a15f171516967e5db9cbc27b4d48763dac37972
Gerrit Hoskins <39583066+GerritHoskins@users.noreply.github.com>
2023-11-24 07:51:39 +0000

    sd3

--- index.js
+++ index.js
@@ -1,4 +1,55 @@
+let mobilenet = undefined;
+let gatherDataState = "STOP_DATA_GATHER";
+let videoPlaying = false;
+let trainingDataInputs = [];
+let trainingDataOutputs = [];
+let examplesCount = [];
+let predict = false;
+
+function enableCam() {
+  // TODO: Fill this out later in the codelab!
+}
+
+
+function trainAndPredict() {
+  // TODO: Fill this out later in the codelab!
+}
+
+
+function reset() {
+  // TODO: Fill this out later in the codelab!
+}
+
+
+
+function gatherDataForClass() {
+  // TODO: Fill this out later in the codelab!
+}
+
 (async () => {
+    const STATUS = document.getElementById('status');
+    const VIDEO = document.getElementById('webcam');
+    const ENABLE_CAM_BUTTON = document.getElementById('enableCam');
+    const RESET_BUTTON = document.getElementById('reset');
+    const TRAIN_BUTTON = document.getElementById('train');
+    const MOBILE_NET_INPUT_WIDTH = 224;
+    const MOBILE_NET_INPUT_HEIGHT = 224;
+    const STOP_DATA_GATHER = -1;
+    const CLASS_NAMES = [];
+
+    ENABLE_CAM_BUTTON.addEventListener('click', enableCam);
+    TRAIN_BUTTON.addEventListener('click', trainAndPredict);
+    RESET_BUTTON.addEventListener('click', reset);
+
+    let dataCollectorButtons = document.querySelectorAll('button.dataCollector');
+    for (let i = 0; i < dataCollectorButtons.length; i++) {
+      dataCollectorButtons[i].addEventListener('mousedown', gatherDataForClass);
+      dataCollectorButtons[i].addEventListener('mouseup', gatherDataForClass);
+      // Populate the human readable names for classes.
+      CLASS_NAMES.push(dataCollectorButtons[i].getAttribute('data-name'));
+    }
+
+
     const status = document.getElementById('status');
     if (status) {
       status.innerText = 'Loaded TensorFlow.js - version: ' + tf.version.tfjs;
