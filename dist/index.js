7e5336d958866ca6e3155bbeb7f5a5190ec515b2
Gerrit Hoskins <39583066+GerritHoskins@users.noreply.github.com>
2023-11-24 07:27:08 +0000

    ssg4

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+import tf from '@tensorflow/tfjs';
+
+(async () => {
+    const status = document.getElementById('status');
+    if (status) {
+      status.innerText = 'Loaded TensorFlow.js - version: ' + tf.version.tfjs;
+    }
+    
+})();
